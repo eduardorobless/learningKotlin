43ecb30575b011f6af7e1ea63d5826d200da5ebd
Eduardo Ignacio Robles Sosa <eduardo_robles14@hotmail.com>
2024-03-06 09:41:02 -0600

    addig top level properties to illustrate visibility

--- src/oo/visibilities.kt
+++ src/oo/visibilities.kt
@@ -4,7 +4,7 @@ package oo
 // protected - same as in java 
 // internal - visible inside same module 
 // public same as in java (Default)
-
+private val i = 42
 
 
 open class Car(val brand: String, private val model: String) {
@@ -29,13 +29,18 @@ class SpecificCar: Car("", "specific") {
 
 fun main(args: Array<String>) {
     val car = Car("Brand", "Model")
-   
+    
+    // only access from class
     println(car.brand)
-    //println(car.model) // error 
-    // println(car.getModel())
+    //println(car.model) //only from the class
+    
 
-
+    // accessing a protected method
     val specificCar = SpecificCar()
     println(specificCar.a())
+
+
+    // accessing top level private property
+    println(i)
  
 }
